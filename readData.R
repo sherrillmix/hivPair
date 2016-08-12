8a67dd342c5c2d46c2f0548a83b0aafa42b96d1a
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-08-12 12:47:35 -0400

    Create pair names and plot AAs

--- readData.R
+++ readData.R
@@ -17,12 +17,20 @@ hiv$sample<-paste(hiv$donorRec,hiv$Pair.ID..)
 hiv$donor<-grepl('Donor',hiv$Donor.or.Recipient)
 hiv$sampleSelect<-paste(hiv$donorRec,hiv$Pair.ID..,hiv$select)
 hiv$sampleFluid<-paste(hiv$donorRec,hiv$Pair.ID..,hiv$fluid)
-write.fa(hiv$Renamed,hiv$seq,'hiv.fa')
+hiv$seqId<-sprintf('seq%d',1:nrow(hiv))
+hiv$baseName<-sub('\\..*$','',hiv$Renamed)
+donors<-with(hiv[hiv$donor,],tapply(baseName,Pair.ID..,unique))
+recs<-with(hiv[!hiv$donor,],tapply(baseName,Pair.ID..,unique))
+pairNames<-mapply(function(x,y)paste(paste(x,collapse='/'),paste(y,collapse='/'),sep='-'),donors,recs)
+write.fa(hiv$seqId,hiv$seq,'hiv.fa')
 
+refName<-'B.FR.83.HXB2_LAI_IIIB_BRU_K03455'
 align<-read.fa('data/AlignSeq.nt.fasta.gz')
+rownames(align)<-align$name
 seqMat<-seqSplit(align$seq)
-refPos<-cumsum(seqMat[1,])
-seqMat<-seqMat[-1,]
+rownames(seqMat)<-align$name
+refPos<-cumsum(seqMat[refName,]!='-')
+seqMat<-seqMat[hiv$seqId,]
 colnames(seqMat)<-sprintf('pos%s.%s',refPos,ave(refPos,refPos,FUN=function(x)1:length(x)))
 nBase<-apply(seqMat,2,function(x)sum(x!='-'))
 startEnd<-range(which(nBase/nrow(seqMat)>.9))
@@ -34,16 +42,16 @@ plotDNA(apply(onlyDiff,1,paste,collapse='')[hiv$donor],groups=paste(hiv$Pair.ID.
 dev.off()
 png('out/align.png',width=2000,height=2000)
 par(mar=c(4,4,1,6))
-plotDNA(apply(seqMat,1,paste,collapse=''),groups=paste(hiv$Pair.ID..,hiv$donor))
+plotDNA(align[hiv$seqId,'seq'],groups=paste(hiv$Pair.ID..,hiv$donor))
 dev.off()
 
 
 aa<-readFaDir('data','^[^LA].*aa.fasta.gz$') #don't read LTR since no AA or AlignSeq
 bigAA<-tapply(aa$seq,aa$name,paste,collapse='')
-bigAA<-bigAA[hiv$Renamed]
 aaMat<-seqSplit(bigAA)
-refPos<-cumsum(aaMat[1,])
-aaMat<-aaMat[-1,]
+rownames(aaMat)<-names(bigAA)
+refPos<-cumsum(aaMat[refName,]!='-')
+aaMat<-aaMat[hiv$seqId,]
 colnames(aaMat)<-sprintf('pos%s.%s',refPos,ave(refPos,refPos,FUN=function(x)1:length(x)))
 nBase<-apply(aaMat,2,function(x)sum(x!='-'))
 seqMat<-seqMat[,nBase>10]
@@ -52,11 +60,11 @@ onlyDiffAA<-aaMat[,apply(aaMat,2,function(x)length(unique(x[x!='-']))>1)]
 
 png('out/diffAA.png',width=2000,height=2000)
 par(mar=c(4,4,1,6))
-plotDNA(apply(onlyDiffAA,1,paste,collapse='')[hiv$donor],groups=paste(hiv$Pair.ID..,hiv$select)[hiv$donor])
+plotAA(apply(onlyDiffAA,1,paste,collapse='')[hiv$donor],groups=paste(hiv$Pair.ID..,hiv$select)[hiv$donor])
 dev.off()
 png('out/alignAA.png',width=2000,height=2000)
 par(mar=c(4,4,1,6))
-plotDNA(apply(aaMat,1,paste,collapse=''),groups=paste(hiv$Pair.ID..,hiv$donor))
+plotAA(apply(aaMat,1,paste,collapse=''),groups=paste(hiv$Pair.ID..,hiv$donor))
 dev.off()
 
 
