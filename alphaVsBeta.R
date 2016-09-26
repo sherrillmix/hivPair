3ec4ba17b59e020b4d2eecc8420fd84d187db8a8
sherrillmix <sherrillmix@users.noreply.github.com>
2016-09-26 17:40:32 -0400

    Start looking at interferon alpha vs beta

---
+++ alphaVsBeta.R
@@ -0,0 +1,30 @@
+if(!exists('hiv'))source("readData.R")
+
+cols<-rainbow.lab(length(unique(hiv$Pair.ID..)),alpha=.7)
+names(cols)<-sort(unique(hiv$Pair.ID..))
+pdf('out/alphaVsBeta.pdf')
+  plot(hiv$IFNa2.Pooled.Donor.cells.IC50..pg..ml.,hiv$IFNbeta.Pooled.Donor.cells.IC50..pg.ml.,log='xy',xaxt='n',yaxt='n',xlab='IFNa2 IC50 (pg/ml)',ylab='IFNbeta IC50 (pg/ml)',bg=cols[as.character(hiv$Pair.ID..)],pch=21,col=NA)
+  segments(min(hiv$IFNa2.Pooled.Donor.cells.IC50..pg..ml.),min(hiv$IFNbeta.Pooled.Donor.cells.IC50..pg.ml.),max(hiv$IFNa2.Pooled.Donor.cells.IC50..pg..ml.),max(hiv$IFNbeta.Pooled.Donor.cells.IC50..pg.ml.))
+  logAxis(1)
+  logAxis(2,las=1)
+  legend('topleft',names(cols),pch=21,pt.bg=cols,inset=.01,title='Pair')
+dev.off()
+
+line<-function(alpha,beta){
+  coefs<-lm(range(log10(hiv$IFNa2.Pooled.Donor.cells.IC50..pg..ml.))~range(log10(hiv$IFNbeta.Pooled.Donor.cells.IC50..pg.ml.)))$coefficients
+  pred<-coefs['(Intercept)']+coefs['range(log10(hiv$IFNbeta.Pooled.Donor.cells.IC50..pg.ml.))']*log10(beta)
+  return(log10(alpha)>pred)
+}
+
+scrambleWithinPatient<-function(alpha,pair){
+  return(ave(alpha,pair,FUN=sample))
+}
+
+aboveLine<-mean(line(hiv$IFNa2.Pooled.Donor.cells.IC50..pg..ml.,hiv$IFNbeta.Pooled.Donor.cells.IC50..pg.ml.))
+if(!exists('scrambleAbove'))scrambleAbove<-replicate(100000,mean(line(scrambleWithinPatient(hiv$IFNa2.Pooled.Donor.cells.IC50..pg..ml.,hiv$Pair.ID..),hiv$IFNbeta.Pooled.Donor.cells.IC50..pg.ml.)))
+pdf('out/aboveLine.pdf')
+  hist(scrambleAbove,xlim=c(.7,1))#range(c(aboveLine,scrambleAbove)))
+  abline(v=aboveLine,col='red')
+  axis(1,seq(.7,.95,.05))
+dev.off()
+
