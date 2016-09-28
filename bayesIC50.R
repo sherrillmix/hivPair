d81c1d14e77c9a643f690ee18a3ee860cfc3a640
sherrillmix <sherrillmix@users.noreply.github.com>
2016-09-28 07:31:06 -0400

    Take out unnecessary hiv variables

--- bayesIC50.R
+++ bayesIC50.R
@@ -66,8 +66,6 @@ targetCols<-c(
   'Env.RT'='Env/RT',
   'Infectivity.RLU.pg.RT...T1249.'='Infectivity (RLU/pg RT)',
   'Replicative.capacity.Pooled.Donor.cells.p24.d7'='Pooled donor\nReplicative capacity (day 7 p24)',
-  'meanRepCap'='Mean replicative capacity\n(proportion of maximum day 7 p24)',
-  'meanIfna'='IFNa2 IC50 (pg/ml)',
   'IFNbeta.Pooled.Donor.cells.IC50..pg.ml.'='IFNbeta IC50 (pg/ml)',
   'IFNa2.Pooled.Donor.cells.IC50..pg..ml.'='IFNa2 IC50 (pg/ml)'
 )
