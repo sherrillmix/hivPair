ab5a35a355e510cbc9dd530eb1d4232da23811f8
sherrillmix <sherrillmix@users.noreply.github.com>
2016-09-29 09:18:42 -0400

    Plot additional components. Fix legend

--- prinComp.R
+++ prinComp.R
@@ -10,7 +10,7 @@ targetCols<-c(
 
 selector<-!apply(is.na(hiv[,names(targetCols)]),1,any)
 pca<-prcomp(hiv[selector,names(targetCols)],scale.=TRUE)
-pcaPoints<-t(t(pca$x[,1:2])/pca$sdev[1:2]/sqrt(nrow(pca$x))) #figure out the point positions based on scores scaled by standard deviations
+pcaPoints<-t(t(pca$x)/pca$sdev/sqrt(nrow(pca$x))) #figure out the point positions based on scores scaled by standard deviations
 importance<-summary(pca)$importance[2,]
 
 cols<-rainbow.lab(length(unique(hiv$donor)),alpha=.8)
@@ -19,17 +19,19 @@ pch<-structure(c(21,22,22),names=c('PL','SE','CV'))
 col=ifelse(hiv[selector,'select']=='BE','black',ifelse(hiv[selector,'select']=='A2','red','#00000055'))
 
 pdf('out/pca.pdf')
-  plot(
-    1,1,type='n',las=1,
-    xlim=range(pcaPoints[,1]),ylim=range(pcaPoints[,2]),
-    xlab=sprintf('Principal component 1 (%d%% of variance)',round(importance[1]*100)),
-    ylab=sprintf('Principal component 2 (%d%% of variance)',round(importance[2]*100))
-  )
-  for(ii in c('UT','A2','BE')){
-    selectSelector<-hiv[selector,'select']==ii
-    points(pcaPoints[selectSelector,],bg=cols[as.character(hiv[selector,'donor'])][selectSelector],pch=pch[hiv[selector,'fluid']][selectSelector],col=col[selectSelector])
+  for(select in list(1:2,2:3,3:4,4:5)){
+    plot(
+      1,1,type='n',las=1,
+      xlim=range(pcaPoints[,select[1]]),ylim=range(pcaPoints[,select[2]]),
+      xlab=sprintf('Principal component %d (%d%% of variance)',select[1],round(importance[select[1]]*100)),
+      ylab=sprintf('Principal component %d (%d%% of variance)',select[2],round(importance[select[2]]*100))
+    )
+    for(ii in c('UT','A2','BE')){
+      selectSelector<-hiv[selector,'select']==ii
+      points(pcaPoints[selectSelector,],bg=cols[as.character(hiv[selector,'donor'])][selectSelector],pch=pch[hiv[selector,'fluid']][selectSelector],col=col[selectSelector])
+    }
+    legend('bottomleft',c('Genital','Plasma','IFNa2 select','IFNb select','Donor','Recipient'),pch=c(22,21,rep(21,4)),col=c('#00000055','#00000055','red','black','#00000055','#00000055'),pt.bg=rep(cols[c('TRUE','FALSE')],c(5,1)),inset=.01)
   }
-  legend('bottomleft',c('Genital','Plasma','IFNa2 select','IFNb select','Donor','Recipient'),pch=c(21,22,rep(21,4)),col=c('#00000055','#00000055','red','black','#00000055','#00000055'),pt.bg=rep(cols[c('TRUE','FALSE')],c(5,1)),inset=.01)
   #plot(pca)
 dev.off()
 
