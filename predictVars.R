de2a4464dee43f97b3133a49f1f5a64999f40996
sherrillmix <sherrillmix@users.noreply.github.com>
2016-10-10 11:03:58 -0400

    Deal with targetCols and missing values

--- predictVars.R
+++ predictVars.R
@@ -2,25 +2,25 @@ library(glmnet)
 library(glmnetPlotR)
 if(!exists('hiv'))source('readData.R')
 
-out<-mclapply(names(selectVars),function(targetCol){
-#for(targetCol in names(selectVars)){
+out<-mclapply(names(targetCols),function(targetCol){
   message(targetCol)
   modelInput<-as.data.frame(onlyDiffAA)
   #modelInput$pair<-as.factor(hiv$Pair.ID..)
   modelMatrix<-model.matrix(formula(sprintf('~ %s',paste(colnames(modelInput),collapse='+'))),modelInput)
-  selector<-hiv$donor
+  selector<-hiv$donor&!is.na(hiv[,targetCol])
   unadjustTarget<-log2(hiv[selector,targetCol])
   target<-unadjustTarget-log2(ave(hiv[selector,targetCol],hiv[selector,'Pair.ID..'],FUN=function(x)mean(x,na.rm=TRUE)))
   fitAlpha<-cv.glmnet(modelMatrix[selector,][!is.na(target),],target[!is.na(target)],nfolds=sum(!is.na(target)),grouped=FALSE)
   #fitBeta<-cv.glmnet(modelMatrix[hiv$donor&!is.na(hiv$IFNbeta.PD.IC50..ng.ml.),],log2(hiv$IFNbeta.PD.IC50..ng.ml.[hiv$donor&!is.na(hiv$IFNbeta.PD.IC50..ng.ml.)]),nfolds=sum(hiv$donor&!is.na(hiv$IFNbeta.PD.IC50..ng.ml.)),grouped=FALSE)
   multiFit<-lapply(unique(hiv$Pair.ID..),function(xx){
-    pairSelect<-hiv[selector,'Pair.ID..']==xx&!is.na(target)
+    pairSelect<-hiv[selector,'Pair.ID..']==xx
+    if(sum(pairSelect)<5||length(unique(target[pairSelect]))==1)return(NULL)
     cv.glmnet(modelMatrix[selector,][pairSelect,],target[pairSelect],nfolds=sum(pairSelect),grouped=FALSE)
   })
   names(multiFit)<-unique(hiv$Pair.ID..)
   pdf(sprintf('out/lasso/%s.pdf',targetCol))
-  plotGlmnet(fitAlpha,markBest1SE=TRUE,main=selectVars[targetCol])
-  plotBetas(fitAlpha$glmnet.fit,ylab=selectVars[targetCol],transformFunc=function(x)2^x,labelLambda=fitAlpha$lambda.1se)
+  plotGlmnet(fitAlpha,markBest1SE=TRUE,main=targetCols[targetCol])
+  plotBetas(fitAlpha$glmnet.fit,ylab=targetCols[targetCol],transformFunc=function(x)2^x,labelLambda=fitAlpha$lambda.1se)
   #plotGlmnet(fitBeta,markBest1SE=TRUE,main='IFN beta IC50')
   #plotBetas(fitBeta$glmnet.fit,ylab=expression('IFN beta IC'[50]%*%' '),transformFunc=function(x)2^x,labelLambda=10^-.3)
   coefs<-coef(fitAlpha)
@@ -35,9 +35,10 @@ out<-mclapply(names(selectVars),function(targetCol){
     }
   }
   for(ii in names(multiFit)){
+    if(is.null(multiFit[[ii]]))next()
     message(ii)
-    plotGlmnet(multiFit[[ii]],markBest1SE=TRUE,main=sprintf('Pair %s %s',ii,sub('\n',' ',selectVars[targetCol])))
-    plotBetas(multiFit[[ii]]$glmnet.fit,ylab=selectVars[targetCol],transformFunc=function(x)2^x,labelLambda=multiFit[[ii]]$lambda.1se)
+    plotGlmnet(multiFit[[ii]],markBest1SE=TRUE,main=sprintf('Pair %s %s',ii,sub('\n',' ',targetCols[targetCol])))
+    plotBetas(multiFit[[ii]]$glmnet.fit,ylab=targetCols[targetCol],transformFunc=function(x)2^x,labelLambda=multiFit[[ii]]$lambda.1se)
     coefs<-coef(multiFit[[ii]])
     coefs<-coefs[coefs[,1]!=0,]
     coefs<-coefs[names(coefs)!="(Intercept)"]
