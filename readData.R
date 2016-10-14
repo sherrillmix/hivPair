44c4773ca71d4fb69ec58c6eb53740f0c35d1044
sherrillmix <sherrillmix@users.noreply.github.com>
2016-10-14 16:18:32 -0400

    Get precise vres and censoring

--- readData.R
+++ readData.R
@@ -108,4 +108,18 @@ targetColCensorDown['Autologous.IC50']<-20
 targetColCensorDown['Bnaber.IC50']<-20
 goodTargetCols<-targetCols[1:5]
 
+#Read in precise vres values to figure out which are censored
+vresPrecise<-read.csv('data/Vres data for Scott.csv',stringsAsFactors=FALSE)
+vresPrecise<-vresPrecise[apply(is.na(vresPrecise),1,sum)==0,]
+rownames(vresPrecise)<-vresPrecise$Renamed
+vresPrecise$isCensor<-vresPrecise$IFN.beta.Pooled.Donor.p24.at.0.44.pg.ml==.1
+hiv$vres<-vresPrecise[hiv$Renamed,'IFN.beta.Pooled.Donor.Vres.at.0.44.pg.ml...UT.']
+hiv$vresCensor<-vresPrecise[hiv$Renamed,'isCensor']
+if(any(is.na(hiv$vres)))stop(simpleError('Unknown vres censoring'))
+if(any(is.na(hiv$vresCensor)))stop(simpleError('Unknown vres censoring'))
 
+hiv$minVres<-.1/hiv$IFN.beta.expt.Replicative.capacity..p24...d7.ng.ml.*100
+hiv$isMinVres<-round(hiv$vres,2)<=round(hiv$minVres,2)
+
+if(any(hiv$vres<hiv$minVres))stop(simpleError('Vres < min Vres'))
+if(any(round(hiv$vres,5)<=round(hiv$minVres,5)&!hiv$vresCensor))stop(simpleError('Vres == min Vres and not censored'))
