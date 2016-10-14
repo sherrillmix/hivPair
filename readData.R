7e321e9e787584f6edfd57cffa7cf4565f76ebcc
sherrillmix <sherrillmix@users.noreply.github.com>
2016-10-14 17:04:52 -0400

    Move seq stuff to own file. Make better names for AA

--- readData.R
+++ readData.R
@@ -42,51 +42,6 @@ hiv$propPD<-hiv$Replicative.capacity.Pooled.Donor.cells.p24.d7/hiv$maxPD
 hiv$meanRepCap<-(hiv$propSD+hiv$propPD)/2
 write.csv(hiv[,c('Renamed','Original.name','Pair.ID..','select','Replicative.capacity.Single.Donor.cells.p24.d7','Replicative.capacity.Pooled.Donor.cells.p24.d7','maxSD','maxPD','propSD','propPD','meanRepCap')],'out/repCap.csv')
 
-
-refName<-'B.FR.83.HXB2_LAI_IIIB_BRU_K03455'
-align<-read.fa('data/AlignSeq.nt.fasta.gz')
-rownames(align)<-align$name
-seqMat<-seqSplit(align$seq)
-rownames(seqMat)<-align$name
-refPos<-cumsum(seqMat[refName,]!='-')
-seqMat<-seqMat[hiv$seqId,]
-colnames(seqMat)<-sprintf('pos%s.%s',refPos,ave(refPos,refPos,FUN=function(x)1:length(x)))
-nBase<-apply(seqMat,2,function(x)sum(x!='-'))
-startEnd<-range(which(nBase/nrow(seqMat)>.9))
-seqMat<-seqMat[,nBase>10&1:ncol(seqMat) %in% startEnd[1]:startEnd[2]]
-onlyDiff<-seqMat[,apply(seqMat,2,function(x)length(unique(x[x!='-']))>1)]
-
-png('out/diff.png',width=2000,height=2000)
-par(mar=c(4,4,1,6))
-plotDNA(apply(onlyDiff,1,paste,collapse='')[hiv$donor],groups=paste(hiv$Pair.ID..,hiv$select)[hiv$donor])
-dev.off()
-png('out/align.png',width=2000,height=2000)
-par(mar=c(4,4,1,6))
-plotDNA(align[hiv$seqId,'seq'],groups=paste(hiv$Pair.ID..,hiv$donor))
-dev.off()
-
-
-aa<-readFaDir('data','^[^LA].*aa.fasta.gz$') #don't read LTR since no AA or AlignSeq
-bigAA<-tapply(aa$seq,aa$name,paste,collapse='')
-aaMat<-seqSplit(bigAA)
-rownames(aaMat)<-names(bigAA)
-refPos<-cumsum(aaMat[refName,]!='-')
-aaMat<-aaMat[hiv$seqId,]
-colnames(aaMat)<-sprintf('pos%s.%s',refPos,ave(refPos,refPos,FUN=function(x)1:length(x)))
-nBase<-apply(aaMat,2,function(x)sum(x!='-'))
-seqMat<-seqMat[,nBase>10]
-onlyDiffAA<-aaMat[,apply(aaMat,2,function(x)length(unique(x[x!='-']))>1)]
-
-
-png('out/diffAA.png',width=2000,height=2000)
-par(mar=c(4,4,1,6))
-plotAA(apply(onlyDiffAA,1,paste,collapse='')[hiv$donor],groups=paste(hiv$Pair.ID..,hiv$select)[hiv$donor])
-dev.off()
-png('out/alignAA.png',width=2000,height=2000)
-par(mar=c(4,4,1,6))
-plotAA(apply(aaMat,1,paste,collapse=''),groups=paste(hiv$Pair.ID..,hiv$donor))
-dev.off()
-
 source('readVarGlyco.R')
 
 targetCols<-c(
