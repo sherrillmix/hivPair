8b0c04326ebbfcaef51c9fd91469d62d0b185ca9
sherrillmix <sherrillmix@users.noreply.github.com>
2016-12-12 12:25:27 -0500

    Clean up colnames and plotting

--- prinComp.R
+++ prinComp.R
@@ -11,62 +11,6 @@ expandedHull<-function(xys,magnification=1,type=c('convex','ellipse')){
 }
 
 selector<-!apply(is.na(hiv[,goodTargetCols]),1,any)
-tmp<-hiv[selector,goodTargetCols]
-rownames(tmp)<-hiv[selector,'Name']
-pca<-prcomp(tmp,scale.=TRUE)
-pcaPoints<-t(t(pca$x)/pca$sdev/sqrt(nrow(pca$x))) #figure out the point positions based on scores scaled by standard deviations
-importance<-summary(pca)$importance[2,]
-cols<-rainbow(length(unique(hiv$fluidSelectDonor)),alpha=.6)
-cols2<-rainbow(length(unique(hiv$fluidSelectDonor)),alpha=.8)
-cols3<-rainbow(length(unique(hiv$fluidSelectDonor)),alpha=.02)
-names(cols)<-names(cols2)<-names(cols3)<-sort(unique(hiv$fluidSelectDonor))
-
-pdf('out/pca.pdf',width=5,height=5)
-  for(select in list(1:2,2:3,3:4,4:5)){
-    for(hullType in c('ellipse','convex')){
-    xlim <- range(-pcaPoints[,select])
-    ylim <- range(-pcaPoints[,select])
-    pcaArrows<-t(t(-pca$rotation[,select])*pca$sdev[select]*sqrt(nrow(pca$x)))	#figure out the arrow positions based on loadings scaled by sdev
-    xlimArrow<-range(pcaArrows[,1])
-    ylimArrow<-range(pcaArrows[,2])
-    ratio <- max(xlimArrow/xlim, ylimArrow/ylim)
-    par(mar=c(3.5,3.5,.1,.1))
-    plot(
-      1,1,type='n',las=1,
-      #xlim=range(-pcaPoints[,select[1]]),ylim=range(-pcaPoints[,select[2]]),
-      xlim=xlim*1.1,ylim=ylim*1.1,mgp=c(2.4,.7,0),
-      xlab=sprintf('Principal component %d (%d%% of variance)',select[1],round(importance[select[1]]*100)),
-      ylab=sprintf('Principal component %d (%d%% of variance)',select[2],round(importance[select[2]]*100))
-    )
-    arrows(0,0,pcaArrows[,1]/ratio,pcaArrows[,2]/ratio,length=.1) #draw arrows
-    arrowText<-dimnames(pcaArrows)[[1]] #get rownames of loadings for labels
-    par(lheight=.7)
-    text(pcaArrows/ratio,sub(' ','\n',sub('\\(.*$','',targetCols[rownames(pcaArrows)])),cex=.8) #label the arrows
-    donorFluidBinary<-list('DO GE UT'=c(TRUE,TRUE),'DO PL UT'=c(TRUE,FALSE),'RE PL UT'=c(FALSE,FALSE))
-    for(dfName in names(donorFluidBinary)){
-      donorFluid<-donorFluidBinary[[dfName]]
-      donorFluidSelector<-hiv[selector,'isDonor']==donorFluid[1]&hiv[selector,'isGenital']==donorFluid[2]&hiv[selector,'Selection']=='UT'
-      hull<-expandedHull(-pcaPoints[donorFluidSelector,select],ifelse(hullType=='ellipse',1.02,1.1),hullType)
-      polygon(hull,border=cols2[dfName],col=cols3[dfName],lwd=2.4)
-    }
-    points(-pcaPoints[,select[1]],-pcaPoints[,select[2]],bg=cols[as.character(hiv[selector,'fluidSelectDonor'])],col=cols2[as.character(hiv[selector,'fluidSelectDonor'])],pch=21,cex=1.5)
-    recSelect<-!hiv[selector,'isDonor']&hiv[selector,'Fluid']=='PL'&hiv[selector,'Selection']=='UT'
-    donSelect<-hiv[selector,'isDonor']&hiv[selector,'Fluid']=='PL'&hiv[selector,'Selection']=='UT'
-    centroids<-cbind(tapply(pcaPoints[recSelect,select[1]],hiv[selector,'Pair ID'][recSelect],mean),tapply(pcaPoints[recSelect,select[2]],hiv[selector,'Pair ID'][recSelect],mean))
-    text(-centroids,rownames(centroids),cex=.8)
-    for(pair in rownames(centroids)){
-      segments(-centroids[pair,1],-centroids[pair,2],-pcaPoints[hiv[selector,'Pair ID']==pair&recSelect,select[1]],-pcaPoints[hiv[selector,'Pair ID']==pair&recSelect,select[2]],col='#00000033',lwd=.5)
-      segments(-centroids[pair,1],-centroids[pair,2],-pcaPoints[hiv[selector,'Pair ID']==pair&donSelect,select[1]],-pcaPoints[hiv[selector,'Pair ID']==pair&donSelect,select[2]],col='#00000033',lwd=.5,lty=2)
-    }
-    legend('topright',names(cols),pch=21,col=cols2,pt.bg=cols,inset=.01,pt.cex=1.5)
-    }
-    biplot(pca,choices=select,cex=.25,xlim=-xlim,ylim=-ylim)
-    biplot(pca,choices=select,cex=.25)
-  }
-dev.off()
-
-
-selector<-!apply(is.na(hiv[,goodTargetCols]),1,any)
 xFlip<--1
 yFlip<- -1
 subselect<-hiv[selector,]$Selection=='UT'
@@ -75,11 +19,10 @@ pca<-prcomp(scaled,scale.=FALSE)
 pcaPoints<-t(t(scaled %*% pca$rotation)/pca$sdev/sqrt(nrow(pca$x))) #figure out the point positions based on scores scaled by standard deviations
 importance<-summary(pca)$importance[2,]
 cols<-rainbow(length(unique(hiv$fluidSelectDonor)),alpha=.6)
-pch<-structure(c(21,21,21),names=c('PL','SE','CV'))
 cols2<-rainbow(length(unique(hiv$fluidSelectDonor)),alpha=.8)
 cols3<-rainbow(length(unique(hiv$fluidSelectDonor)),alpha=.02)
 names(cols)<-names(cols2)<-names(cols3)<-sort(unique(hiv$fluidSelectDonor))
-pdf('out/pca2.pdf',width=5,height=5)
+pdf('out/pca.pdf',width=5,height=5)
   for(subfig2 in c(FALSE,TRUE)){
   select<-1:2
   xlim <- ylim <- range(yFlip*pcaPoints[,select])
@@ -99,9 +42,9 @@ pdf('out/pca2.pdf',width=5,height=5)
     arrows(0,0,xFlip*pcaArrows[,1]/ratio,yFlip*pcaArrows[,2]/ratio,length=.1) #draw arrows
     arrowText<-dimnames(pcaArrows)[[1]] #get rownames of loadings for labels
     par(lheight=.7)
-    text(xFlip*pcaArrows[,1]/ratio,yFlip*pcaArrows[,2]/ratio,sub(' ','\n',sub('\\(.*$','',targetCols[rownames(pcaArrows)])),cex=.75) #label the arrows
+    text(xFlip*pcaArrows[,1]/ratio,yFlip*pcaArrows[,2]/ratio,sub(' ','\n',sub('\\(.*$','',rownames(pcaArrows))),cex=.75) #label the arrows
   }
-  donorFluidBinary<-list('DO GE UT'=c(TRUE,TRUE),'DO PL UT'=c(TRUE,FALSE),'RE PL UT'=c(FALSE,FALSE))
+  donorFluidBinary<-list('Donor Genital Untreated'=c(TRUE,TRUE),'Donor Plasma Untreated'=c(TRUE,FALSE),'Recipient Plasma Untreated'=c(FALSE,FALSE))
   for(dfName in names(donorFluidBinary)){
     donorFluid<-donorFluidBinary[[dfName]]
     donorFluidSelector<-hiv[selector,'isDonor']==donorFluid[1]&hiv[selector,'isGenital']==donorFluid[2]&hiv[selector,'Selection']=='UT'
@@ -111,7 +54,7 @@ pdf('out/pca2.pdf',width=5,height=5)
   if(subfig2)thisSelect<-!subselect
   else thisSelect<-subselect
   points(xFlip*pcaPoints[,select[1]][thisSelect],yFlip*pcaPoints[,select[2]][thisSelect],bg=cols[as.character(hiv[selector,'fluidSelectDonor'])[thisSelect]],col=cols2[as.character(hiv[selector,'fluidSelectDonor'])[thisSelect]],pch=21,cex=1.5)
-  if(subfig2) legend('topright',names(cols),pch=21,col=cols2,pt.bg=cols,inset=.01,pt.cex=1.5)
+  if(subfig2) legend('topright',names(cols),pch=21,col=cols2,pt.bg=cols,inset=.01,pt.cex=1.5,cex=.7)
   }
 dev.off()
 
@@ -136,18 +79,6 @@ ids<-list(
   ,recipient=sampleNames[!xx$isDonor&xx$Selection=='UT']
 )
 
-distList<-lapply(ids,function(xx,distMat,recipientIds){
-  select<-distMat[xx,recipientIds]
-  dists<-select[upper.tri(select)]
-  return(dists)
-},distMat,ids[['recipient']])
-distList<-distList[order(sapply(distList,mean),decreasing=TRUE)]
-
-pdf('out/dists.pdf')
-par(mar=c(7.2,4,.1,.1))
-  vpPlot(factor(rep(names(distList),sapply(distList,length)),levels=names(distList)),unlist(distList),las=3,cex=.5,col=NA,bg='#00000066',pch=21,ylab='Distance to recipient samples')
-dev.off()
-
 pairRecDist<-unlist(ave(split(cbind(scaled,!hiv[selector,'isDonor']&hiv[selector,'Fluid']=='PL'&hiv[selector,'Selection']=='UT'),1:nrow(scaled)),hiv[selector,'Pair ID'],FUN=function(df){
   df<-do.call(rbind,df)
   select<-df[,ncol(df)]==1
@@ -171,7 +102,7 @@ pdf('out/centroidDist.pdf')
   legend('topright',names(cols),pch=21,pt.bg=cols,col=NA,inset=.01,title='Pair',ncol=2,cex=2)
   pos<-vpPlot(factor(hiv[selector,'fluidSelectDonor'],levels=unique(hiv$fluidSelectDonor[order(hiv$isDonor,hiv$Selection=='UT',hiv$Fluid=='PL',hiv$Selection=='A2',decreasing=TRUE)])),pairRecDist,las=3,col=NA,bg=cols[as.character(hiv[selector,'Pair ID'])],pch=21,ylab='Distance to within-pair recipient centroid',las=2)
   legend('topright',names(cols),pch=21,pt.bg=cols,col=NA,inset=.01,title='Pair',ncol=2)
-  text(pos,pairRecDist,hiv[selector,'Renamed'],cex=.2) 
+  text(pos,pairRecDist,hiv[selector,'Name'],cex=.2) 
 dev.off()
 
 
--- roc.R
+++ roc.R
@@ -9,30 +9,18 @@ pdf('out/roc.pdf',width=4.5,height=4.5)
     thisSelect<-selector&hiv$Selection=='UT'&(hiv$isGenital==genital|!hiv$isDonor)
     preds<-lapply(goodTargetCols,function(xx)prediction(hiv[thisSelect,xx], !hiv[thisSelect,'isDonor']))
     aucCI<-lapply(goodTargetCols,function(xx)ci(!hiv[thisSelect,'isDonor'],hiv[thisSelect,xx]))
-    sensCI<-lapply(goodTargetCols,function(xx)ci.se(roc(!hiv[thisSelect,'isDonor'],hiv[thisSelect,xx]),specificities=seq(0,1,.01)))
+    sensCI<-lapply(goodTargetCols,function(xx)ci.se(roc(!hiv[thisSelect,'isDonor'],hiv[thisSelect,xx]),specificities=seq(0,1,.01),progress='text'))
     names(preds)<-goodTargetCols
     perfs<-lapply(preds,function(pred)performance(pred, "tpr", "fpr"))
     aucs<-lapply(preds,function(pred)performance(pred, "auc")@y.values[[1]])
     cols<-rainbow(length(perfs),alpha=.7)
     cols2<-rainbow(length(perfs),alpha=.3)
+    names(cols)<-names(cols2)<-names(perfs)
     par(mar=c(3.5,3.5,1,.1))
     plot(1,1,type='n',xlim=0:1,ylim=0:1,xlab=perfs[[1]]@x.name,ylab=perfs[[1]]@y.name,las=1,mgp=c(2.2,.5,0),main=ifelse(genital,'Genital','Plasma'))
     for(ii in 1:length(perfs))lines(perfs[[ii]]@x.values[[1]],perfs[[ii]]@y.values[[1]],col=cols[[ii]],lw=3)
     for(ii in 1:length(perfs))polygon(1-c(seq(0,1,.01),rev(seq(0,1,.01))),c(sensCI[[ii]][,'2.5%'],rev(sensCI[[ii]][,'97.5%'])),col=cols2[[ii]],border=NA)
     abline(0,1,lty=2)
-    legend('bottomright',sub('\\(.*$','',targetCols[names(perfs)]),col=cols,lwd=2,adj=c(0,.5),bty='n')
-  }
-  for(treat in unique(hiv$Selection[hiv$Selection!='UT'])){
-    thisSelect<-selector&((hiv$isDonor&hiv$Selection==treat)|(!hiv$isDonor&hiv$Selection=='UT'))&!hiv$isGenital
-    preds<-lapply(goodTargetCols,function(xx)prediction(hiv[thisSelect,xx], !hiv[thisSelect,'isDonor']))
-    names(preds)<-goodTargetCols
-    perfs<-lapply(preds,function(pred)performance(pred, "tpr", "fpr"))
-    aucs<-lapply(preds,function(pred)performance(pred, "auc")@y.values[[1]])
-    cols<-rainbow(length(perfs),alpha=.7)
-    par(mar=c(3.5,3.5,1,.1))
-    plot(1,1,type='n',xlim=0:1,ylim=0:1,xlab=perfs[[1]]@x.name,ylab=perfs[[1]]@y.name,las=1,mgp=c(2.2,.5,0),main=sprintf('%s donor vs UT recipient',treat))
-    for(ii in 1:length(perfs))lines(perfs[[ii]]@x.values[[1]],perfs[[ii]]@y.values[[1]],col=cols[[ii]],lw=3)
-    abline(0,1,lty=2)
-    legend('bottomright',sub('\\(.*$','',targetCols[names(perfs)]),col=cols,lwd=2,adj=c(0,.5),bty='n')
+    legend('bottomright',sub('\\(.*$','',names(perfs)),col=cols,lwd=2,adj=c(0,.5),bty='n')
   }
 dev.off()
