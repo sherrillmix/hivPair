d6d8460eabd350b20730fea8b2e59a1d50c958cf
sherrillmix <sherrillmix@users.noreply.github.com>
2016-12-20 13:17:49 -0500

    Add script to run everything

---
+++ runAll.R
@@ -0,0 +1,9 @@
+source('readData.R')
+#generate box and whisker plots
+source('boxWhisker.R')
+#generate PCA plots
+source('prinComp.R')
+#generate ROC plots
+source('roc.R')
+#run Bayesian models
+source('bayesIC50.R')
